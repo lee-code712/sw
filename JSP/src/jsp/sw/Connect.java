a260db79fd2368c039d4555e9b3048d626e617a8
yuriLee <yurilee07@naver.com>
2021-02-04 15:42:24 +0900

    connect.java 매개변수 있는 함수 추가

--- JSP/src/jsp/sw/Connect.java
+++ JSP/src/jsp/sw/Connect.java
@@ -22,5 +22,25 @@ public class Connect {
          
         return conn;
     }
+	
+	public Connection getConnection(String univ_id) {
+        Connection conn = null;
+         
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url ="jdbc:oracle:thin:@localhost:1521:xe";
+            String user ="dwu";
+            String password ="dwu_pass";
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("driver load 실패!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("DB 연결 실패!");
+            e.printStackTrace();
+        }
+         
+        return conn;
+    }
 
 }
